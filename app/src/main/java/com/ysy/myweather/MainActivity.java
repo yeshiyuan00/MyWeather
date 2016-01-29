d6cb95fed369be6e6ed5e7109035e8ce93c56047
yeshiyuan <yeshiyuan00@gmail.com>
2016-01-29 14:53:28 +0800

    add listview to layout

--- app/src/main/java/com/ysy/myweather/MainActivity.java
+++ app/src/main/java/com/ysy/myweather/MainActivity.java
@@ -1,12 +1,18 @@
 package com.ysy.myweather;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static class PlaceholderFragment extends Fragment {
+
+        private ListView listView;
+
+        private String[] forcecastArray = new String[]{"Today-Sunny-88/63", "tomorrow-Foggy-70/46",
+                "Wends-Cloudy-72/63", "Thurs-Rainy-64/51", "Fri-Foggy-70/46", "Sat-Sunny-76/68"};
+        List<String> weekForcecast = new ArrayList<String>(Arrays.asList(forcecastArray));
+
         public PlaceholderFragment() {
 
         }
@@ -26,8 +39,12 @@ public class MainActivity extends AppCompatActivity {
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_main, container, false);
-            return view;
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+             ArrayAdapter mForcecastAdapter = new ArrayAdapter(getActivity(), R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview, weekForcecast);
+            listView= (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForcecastAdapter);
+            return rootView;
         }
     }
 }
