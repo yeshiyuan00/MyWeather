c66d4a5a0e83cb52c3358b2870ad3423fcbe9dd6
yeshiyuan <yeshiyuan00@gmail.com>
2016-02-25 15:03:14 +0800

    Adding a CursorAdapter

--- app/src/main/java/com/ysy/myweather/ForecastFragment.java
+++ app/src/main/java/com/ysy/myweather/ForecastFragment.java
@@ -1,33 +1,31 @@
 package com.ysy.myweather;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.ysy.myweather.data.WeatherContract;
-
-import java.util.ArrayList;
 
 /**
  * User: ysy
  * Date: 2016/2/18
  * Time: 16:54
  */
-public class ForecastFragment extends Fragment {
+public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int FORECAST_LOADER = 0;
 
     private ForecastAdapter mForcecastAdapter;
 
@@ -36,6 +34,7 @@ public class ForecastFragment extends Fragment {
     public ForecastFragment() {
 
     }
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -61,21 +60,18 @@ public class ForecastFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        String locationSetting = Utility.getPreferredLocation(getActivity());
-
-        // Sort order:  Ascending, by date.
-        String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
-        Uri weatherForLocationUri = WeatherContract
-                .WeatherEntry.buildWeatherLocationWithStartDate(locationSetting,
-                        System.currentTimeMillis());
-        Cursor cursor = getActivity().getContentResolver()
-                .query(weatherForLocationUri, null, null, null, sortOrder);
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        mForcecastAdapter = new ForecastAdapter(getActivity(), cursor, 0);
+        mForcecastAdapter = new ForecastAdapter(getActivity(), null, 0);
         listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForcecastAdapter);
 
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        getLoaderManager().initLoader(FORECAST_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
     }
 
     private void updateWeather() {
@@ -91,4 +87,25 @@ public class ForecastFragment extends Fragment {
     }
 
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String locationSetting = Utility.getPreferredLocation(getActivity());
+
+        // Sort order:  Ascending, by date.
+        String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
+        Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+                locationSetting, System.currentTimeMillis());
+
+        return new CursorLoader(getActivity(), weatherForLocationUri, null, null, null, sortOrder);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mForcecastAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mForcecastAdapter.swapCursor(null);
+    }
 }
